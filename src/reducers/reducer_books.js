09592d0224f90c86f697432688912a9bd0bdb8fc
Robert Powers <bobpowers@me.com>
2018-04-13 13:11:50 -0500

    Completed starter Redux project

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
-    return [
-        {title: 'Javascript: The Good Parts'},
-        {title: 'Harry Potter'},
-        {title: 'The Dark Tower'},
-        {title: 'Eloquent Ruby'}
-    ]
+	return [
+		{ title: 'Javascript: The Good Parts', pages: 101 },
+		{ title: 'Harry Potter', pages: 46 },
+		{ title: 'The Dark Tower', pages: 98 },
+		{ title: 'Eloquent Ruby', pages: 2 }
+	];
 }
